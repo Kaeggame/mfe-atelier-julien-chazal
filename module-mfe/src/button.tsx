6b43f091a1ac9d76e99f23363a08512e2a2c1b77
Flamby <66632621+Kaeggame@users.noreply.github.com>
2025-02-13 10:37:36 +0100

    header working

--- module-mfe/src/button.tsx
+++ module-mfe/src/button.tsx
@@ -1,4 +1,11 @@
 // src/button.tsx
 export default function Button() {
-    return <div>Provider button</div>;
-  }
+  return (
+    <div style={{width: '100%', height: '50px', background: '#dccac7', color: 'black', display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
+      <button style={{textDecoration: 'none'}}>Accueil</button>
+      <button style={{textDecoration: 'none'}}>Services</button>
+      <button style={{textDecoration: 'none'}}>À propos</button>
+      <button style={{textDecoration: 'none'}}>Contact</button>
+    </div>
+  );
+}
